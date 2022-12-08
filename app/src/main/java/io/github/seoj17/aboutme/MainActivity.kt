4143f1ed15c718b5ce0db7701af5833d321740f1
SEO-J17 <qw4643987@naver.com>
2022-12-08 19:13:14 +0900

    :sparkles: 메인 뷰 구현함

--- app/src/main/java/io/github/seoj17/aboutme/MainActivity.kt
+++ app/src/main/java/io/github/seoj17/aboutme/MainActivity.kt
@@ -2,10 +2,46 @@ package io.github.seoj17.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.github.seoj17.aboutme.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var presenter: MainContract.Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        presenter = MainPresenter(this)
     }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.start()
+    }
+
+    override fun showInputName() {
+        with(binding) {
+            nickName.visibility = View.VISIBLE
+            submitNickName.setOnClickListener {
+                presenter.doneInputName("${nickName.text}")
+            }
+        }
+    }
+
+    override fun hideInputName() {
+        binding.nickName.visibility = View.GONE
+    }
+
+    override fun showName(name: String) {
+        with(binding.realName) {
+            visibility = View.VISIBLE
+            text = name
+        }
+    }
+
+    override fun hideName() {
+        binding.realName.visibility = View.GONE
+    }
+
 }
